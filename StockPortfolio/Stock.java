b22ac6f459268a71c46efb372ec50f9026eaa436
AshishKerikere <93969829+AshishKerikere@users.noreply.github.com>
2022-11-06 20:21:45 +0530

    Stock class

---
+++ StockPortfolio/Stock.java
@@ -0,0 +1,12 @@
+public class Stock {
+    String name_ofStock;
+    int number_ofShare;
+    double price_ofShare;
+    double total_Value_ofStock;
+
+    public Stock(String name_ofStock,int number_ofShare, double price_ofShare){
+        this.name_ofStock = name_ofStock;
+        this.number_ofShare = number_ofShare;
+        this.price_ofShare = price_ofShare;
+    }
+}
