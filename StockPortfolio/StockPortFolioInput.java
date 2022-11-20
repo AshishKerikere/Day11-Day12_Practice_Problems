3bd2653323f23b314544cf7c8b57ebd7938bca02
AshishKerikere <93969829+AshishKerikere@users.noreply.github.com>
2022-11-20 20:27:38 +0530

    Stock Portfolio Input

--- StockPortfolio/StockPortFolioInput.java
+++ StockPortfolio/StockPortFolioInput.java
@@ -1,12 +1,24 @@
+import java.util.Scanner;
 public class StockPortFolioInput {
     public static void main(String[] args) {
 
-       /* Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the Number of stocks in Portfolio");
-        int number_ofStocks = sc.nextInt();*/
+        Scanner scanner = new Scanner(System.in);
 
-        StockPortfolio stockPortfolio = new StockPortfolio(3);
-        stockPortfolio.fillPortfolio();// Method to fill the details of Portfolio
-        stockPortfolio.printPortfolio();
+        System.out.println("Enter the number of Stocks");
+        int number= scanner.nextInt();
+
+        StockPortfolio stockPortfolio = new StockPortfolio("Ashish's Stocks", number);
+        for (int i = 0; i < 3; i++){
+            System.out.println("Enter the name of the Stock");
+            String s = scanner.next();
+            System.out.println("Enter the number of Shares");
+            int k = scanner.nextInt();
+            System.out.println("Enter the Price of the Shares");
+            double d = scanner.nextDouble();
+            stockPortfolio.stock[i] = new Stock(s, k, d);
+        }
+
+        stockPortfolio.calculateTotalValueOfStock();
+        stockPortfolio.printStockPortfolio();
     }
 }
